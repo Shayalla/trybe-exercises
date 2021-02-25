42dd8c7783b3bbbddf4ea4ffc40a26f55fd2dbe0
Jessie <miss_sc_jessi@hotmail.com>
2021-02-25 18:57:03 -0300

    exercicios1 ao 5

---
+++ bloco_4/Array e For.js
@@ -0,0 +1,72 @@
+//Arrays 
+
+//let menu = ['Home', 'Serviços', 'Portfólio', 'Portfólio'];
+// let menuServices = menu[1] ;
+
+// console.log(menuServices);
+
+// let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+// let indexOfPortfolio = menu.indexOf('Portfólio');
+
+// console.log(indexOfPortfolio);
+
+// let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+
+// menu.push('Contato');
+
+// console.log(menu);
+
+//FOR
+
+// let groceryList = ["Arroz", "Feijão", "Alface", "Melancia"];
+
+// for (let i = 0; i < groceryList.length; i++){
+//     console.log(groceryList[i]);
+// }
+
+//Exercicios
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+//1
+// for (let i = 0; i < numbers.length; i++){
+//      console.log(numbers[i]);
+// }
+
+//2
+// let soma = 0;
+
+// for (let i = 0; i < numbers.length; i++){
+//     soma = soma + numbers[i]
+// }
+// console.log(soma);
+
+//3 e 4
+
+// let soma = 0;
+
+// for (let i = 0; i < numbers.length; i++) {
+   
+//     soma = soma + numbers[i];
+   
+// }
+// let resultado = soma / numbers.length-1;
+
+// if (resultado > 20){
+//     resultado = 'Valor maior que 20'
+// } else {
+//     resultado = 'Valor menor que 20'
+// }
+// console.log(resultado);
+
+// 5
+// let maior = 0;
+// for (let i = 0; i < numbers.length; i++) {
+//     if (numbers[i] > maior) {
+//         maior = numbers[i];
+//     }
+// }
+// console.log(maior);
+
+//6
+
