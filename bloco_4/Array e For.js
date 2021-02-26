54f72af6519d63da10f54e68d2cbb5a53d6dfc4a
Jessie <miss_sc_jessi@hotmail.com>
2021-02-26 15:40:57 -0300

    exercicios resolvidos arrumado

--- bloco_4/Array e For.js
+++ bloco_4/Array e For.js
@@ -74,7 +74,6 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
 // for (index = 0; index < numbers.length; index += 1){
 //     if (numbers[index] % 2 !== 0) {
-//         impar = numbers[index];
 //         resultado += 1;    
 //     }
 // }
