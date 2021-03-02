c19608bb19c6efa89e19ef01466039d9ebde7ba3
Jessie <miss_sc_jessi@hotmail.com>
2021-03-02 13:21:50 -0300

    todos exercicios prontos

--- bloco_4/Objeto e função.js
+++ bloco_4/Objeto e função.js
@@ -41,9 +41,9 @@
 //     }
 //   }
 
-//FUNÇÕES
+// FUNÇÕES
 
-//Crie uma função que receba uma string e retorne true se for um palíndromo , ou false , se não for.
+// Crie uma função que receba uma string e retorne true se for um palíndromo , ou false , se não for.
 
 // function verificaPalindromo(palavra) {
 //     for (var i = 0; i < palavra.length / 2; i++) {
@@ -57,7 +57,7 @@
 //     console.log(verificaPalindromo('arara'));
 //     console.log(verificaPalindromo('desenvolvimento'));
 
-//Crie uma função que receba um array de inteiros e retorne o índice do maior valor.
+// Crie uma função que receba um array de inteiros e retorne o índice do maior valor.
 
 // let array = [2, 3, 6, 7, 10, 1];
 
@@ -72,7 +72,7 @@
 // }
 // console.log(indiceMaiorValor(array));
 
-//Crie uma função que receba um array de inteiros e retorne o índice do menor valor.
+// Crie uma função que receba um array de inteiros e retorne o índice do menor valor.
 // const array = [2, 4, 6, 7, 10, 0, -3]
 
 // function indiceMenorValor(array) {
@@ -86,7 +86,7 @@
 // }
 // console.log(indiceMenorValor(array));
 
-//Crie uma função que receba um array de nomes e retorne o nome com a maior quantidade de caracteres.
+// Crie uma função que receba um array de nomes e retorne o nome com a maior quantidade de caracteres.
 
 // const array = ['José', 'Lucas', 'Nádia', 'Fernanda', 'Cairo', 'Joana'];
 
@@ -103,30 +103,53 @@
 // }
 // console.log(nomeMaior(array));
 
-//Crie uma função que receba um array de inteiros e retorne o inteiro que mais se repete.
+// Crie uma função que receba um array de inteiros e retorne o inteiro que mais se repete.
 
 // const array = [2, 3, 2, 5, 8, 2, 3];
 
-const array = [1, 2, 3, 4, 5]
+// const array = [1, 2, 3, 4, 5]
 
-function numeroMaisRepete(array) {
-  let numeroOrdenado = array.sort();
-  let total = 1;
-  let resultado = 0;
-  let temp = 1;
-  for (let index = 0; index <= numeroOrdenado.length - 1; index += 1) {
-    if (numeroOrdenado[index] === numeroOrdenado[index + 1]) {
-      temp += 1;
+// function numeroMaisRepete(array) {
+//   let numeroOrdenado = array.sort();
+//   let total = 1;
+//   let resultado = 0;
+//   let temp = 1;
+//   for (let index = 0; index <= numeroOrdenado.length - 1; index += 1) {
+//     if (numeroOrdenado[index] === numeroOrdenado[index + 1]) {
+//       temp += 1;
 
-    } else if (temp > total) {
-      resultado = numeroOrdenado[index];
-      total = temp;
-      temp = 1
-    }
-  }
-  if (resultado === 0){
-    return 'Não existe número repetido!'
-  }
-  return resultado;
-}
-console.log(numeroMaisRepete(array));
+//     } else if (temp > total) {
+//       resultado = numeroOrdenado[index];
+//       total = temp;
+//       temp = 1
+//     }
+//   }
+//   if (resultado === 0) {
+//     return 'Não existe número repetido!'
+//   }
+//   return resultado;
+// }
+// console.log(numeroMaisRepete(array));
+
+// Crie uma função que receba um número inteiro N e retorne o somatório de todos os números de 1 até N.
+
+// function somarAte (n){
+
+//   let soma = 0;
+  
+//   for (let index = 1; index <= n; index += 1) {
+//     soma += index;
+//   }
+//   return soma;
+// }
+
+// console.log(somarAte(5));
+
+//Crie uma função que receba uma string word e outra string ending . Verifique se a string ending é o final da string word . Considere que a string ending sempre será menor que a string word .
+
+// function verificaPalavra(word, endingWord) {
+//   let resultado = word.endsWith(endingWord);
+//   return resultado;
+// }
+
+// console.log(verificaPalavra('Trybe', 'be'));
