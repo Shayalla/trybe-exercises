ec3c1b9fa70c9a7e80b0c7b235a8b942ac4093d2
Jessie <miss_sc_jessi@hotmail.com>
2021-03-17 15:19:00 -0300

    respostas e mais bibliotecas

--- Bloco_6/6_1/script.js
+++ Bloco_6/6_1/script.js
@@ -58,4 +58,77 @@ function checaNumero(valor){
             alert("Permitido somente número");
         }
     }
-  }    
+  }  
+  
+  function dataInicio(){
+    let picker = new Pikaday({field:document.querySelector('#datepicker')});
+    picker.toString('DD-MM-YYYY');
+  }
+  dataInicio();
+
+  function isset(str) {
+    return window[str] !== undefined;
+}
+
+function renderCurriculum(event) {
+  event.preventDefault();
+  const formElements = document.querySelectorAll('input');
+  for (let index = 0; index < formElements.length; index += 1) {
+    if (formElements[index].type === 'radio' && !formElements[index].checked) {
+      continue;
+    }
+    const userInput = formElements[index].value;
+    const dataUser = document.querySelector('.form-data');
+    if (checkEmail() && checkData()) {
+      const div = document.createElement('div');
+      div.className = 'div-curriculum';
+      div.innerHTML = userInput;
+      dataUser.appendChild(div);
+    }
+  }
+}
+
+function apertaBotao(){
+  const submitButton = document.querySelector('.submit-button');
+  submitButton.addEventListener('click', renderCurriculum);
+}
+apertaBotao()
+
+function validateData(data) {
+  if (data.indexOf('/') === 2 || data.indexOf('/') === 5) {
+    const day = data.substr(0, 2);
+    const month = data.substr(3, 2);
+    const year = data.substr(6, 4);
+    if ((day > 0 && day < 31) && (month > 0 && month <= 12) && (year > 0 && year.length === 4)) {
+      return true;
+    }
+  }
+  return false;
+}
+validateData();
+
+function checkData() {
+  const inputData = document.querySelector('.input-data');
+  let data = inputData.value;
+  const userData = validateData(data);
+  if (!userData && data.length) {
+    inputData.value = '';
+    alert('data invalida');
+    return false;
+  }
+  return userData;
+}
+checkData();
+
+function checkEmail() {
+  const email = document.querySelector('.email-input');
+  let insertedEmail = email.value;
+  const emailFormat = /^[a-z0-9.]+@[a-z0-9]+\.[a-z]+$/.test(insertedEmail);
+  if (!emailFormat && insertedEmail.length) {
+    email.value = '';
+    alert('email inválido');
+    return false;
+  }
+  return emailFormat
+}
+checkEmail();
