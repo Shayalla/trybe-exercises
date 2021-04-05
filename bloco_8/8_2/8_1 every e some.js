a331f3ed22400ea71fe5e0241a9485aa22cce2b5
Jessie <miss_sc_jessi@hotmail.com>
2021-04-05 20:26:03 -0300

    exercicios da 8.1 e da atual

---
+++ bloco_8/8_2/8_1 every e some.js
@@ -0,0 +1,18 @@
+const listNames = ['Maria', 'Manuela', 'Jorge', 'Ricardo', 'Wilson'];
+
+const verifyFirstLetter = (letter, names) => names.some((name) => name[0] === letter);
+
+// console.log(verifyFirstLetter('J', listNames)); // true
+// console.log(verifyFirstLetter('x', listNames)); // false
+
+const grades = {
+    portugues: 'Aprovado',
+    matematica: 'Reprovado',
+    ingles: 'Aprovado',
+  };
+  
+  const verifyGrades = (studentGrades) => (
+    Object.values(studentGrades).every((grade) => grade === 'Aprovado')
+  );
+  
+  console.log(verifyGrades(grades));
---
+++ bloco_8/8_2/map.js
@@ -0,0 +1,68 @@
+// const persons = [
+//     { firstName: 'Maria', lastName: 'Ferreira' },
+//     { firstName: 'João', lastName: 'Silva' },
+//     { firstName: 'Antonio', lastName: 'Cabral' },
+//   ];
+  
+//   const fullNames = [];
+  
+//   for (let index = 0; index < persons.length; index += 1) {
+//     fullNames.push(`${persons[index].firstName} ${persons[index].lastName}`);
+//   }
+  
+//   console.log(fullNames);
+
+//com map
+
+const persons = [
+    { firstName: 'Maria', lastName: 'Ferreira' },
+    { firstName: 'João', lastName: 'Silva' },
+    { firstName: 'Antonio', lastName: 'Cabral' },
+  ];
+  
+  const fullNames = persons.map((person) => `${person.firstName} ${person.lastName}`);
+  
+//   console.log(fullNames); // [ 'Maria Ferreira', 'João Silva', 'Antonio Cabral' ]
+
+// const numbers = [1, 2, 3, 4, -5];
+
+// const negativeNumbers = [];
+// for (let index = 0; index < numbers.length; index += 1) {
+//   if (numbers[index] > 0) {
+//     negativeNumbers.push(numbers[index] * -1);
+//   } else {
+//     negativeNumbers.push(numbers[index]);
+//   }
+// }
+
+// console.log(negativeNumbers); // [ -1, -2, -3, -4, -5 ]
+// console.log(numbers); // [ 1, 2, 3, 4, -5 ]
+
+//com map
+
+const numbers = [1, 2, 3, 4, -5];
+
+const negativeNumbers = numbers.map((number) => ((number > 0) ? number * (-1) : number));
+
+console.log(negativeNumbers); // [ -1, -2, -3, -4, -5 ]
+console.log(numbers); // [ 1, 2, 3, 4, -5 ]
+
+
+//unir 
+
+const products = ['Arroz', 'Feijao', 'Alface', 'Tomate'];
+
+const prices = [2.99, 3.99, 1.5, 2];
+
+const updateProducts = (listProducts, listPrices) => listProducts.map((product, index) => (
+    { [product]: listPrices[index] }
+  ));
+
+const listProducts = updateProducts(products, prices);
+console.log(listProducts);
+// => [
+//   { Arroz: 2.99 },
+//   { Feijao: 3.99 },
+//   { Alface: 1.5 },
+//   { Tomate: 2 },
+// ]
